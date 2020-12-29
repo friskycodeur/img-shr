ecc405838466657fef75d65b21282172e1ad0498
Prateek Maheshwari <friskycodeur@gmail.com>
2020-12-29 13:16:54 +0530

    minor fixes

--- imgshr/settings.py
+++ imgshr/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'post/media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK= 'bootstrap4'
